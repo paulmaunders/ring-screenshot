7d6870f65277790af2f2fce7230b4323ec6556cc
Paul Maunders <paul@maunders.net>
2018-09-16 21:33:49 +0100

    Update the download filename to include the datetime and ensure the screenshots folder exists

--- ring-screenshot.py
+++ ring-screenshot.py
@@ -1,27 +1,33 @@
 import os
+import time
 from ring_doorbell import Ring
 from pprint import pprint
 
 # Set your environment variables with export RING_USERNAME='user@host'
 RING_USERNAME = os.environ.get('RING_USERNAME', None)
 RING_PASSWORD = os.environ.get('RING_PASSWORD', None)
+SCREENSHOT_DIRECTORY = './screenshots'
 
-pprint(os.environ)
+# Create the screenshot folder
+if not os.path.exists(SCREENSHOT_DIRECTORY):
+    os.makedirs(SCREENSHOT_DIRECTORY)
+
+# Connect to RING's API
 
 myring = Ring(RING_USERNAME, RING_PASSWORD)
-
 myring.is_connected
 True
 
-pprint(myring.devices)
+# List devices
+# pprint(myring.devices)
 
-#Download a video
-
+#Download the most recent motion video from the 2nd stickup cam
 dev = myring.stickup_cams[1]
+# pprint(dev.history(limit=10, kind='motion')[0]['id'])
 
-pprint(dev.history(limit=10, kind='motion')[0]['id'])
+timestr = dev.history(limit=10, kind='motion')[0]['created_at'].strftime("%Y%m%d-%H%M%S")
 
 dev.recording_download(
     dev.history(limit=10, kind='motion')[0]['id'],
-                     filename='./screenshots/lastding.mp4',
+                     filename='./' + SCREENSHOT_DIRECTORY + '/motion-stickup1-' + timestr + '.mp4',
                      override=True)
