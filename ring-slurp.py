b6afaed160ebe2a663e85b1928336731cd1cffc8
Paul Maunders <paul@maunders.net>
2018-10-02 14:37:07 +0100

    Tidy and add comments to ring-slurp script

--- ring-slurp.py
+++ ring-slurp.py
@@ -28,26 +28,41 @@ myring.is_connected
 True
 
 # List devices
-# pprint(myring.devices)
+pprint(myring.devices)
 
 #Download the most recent motion video from the 2nd stickup cam
 dev = myring.stickup_cams[1]
 #old_event=6605114755809561293
-#pprint(dev.history(limit=100, kind='motion', older_than=old_event))
+pprint(dev.id)
+exit()
 
+#TODO!!!
+#Loop through all devices
+#Use dev.id in the filename, store them in sub-folders (1 per device)
+# for dev in list(myring.stickup_cams + myring.chimes + myring.doorbells):
 
-#Loop through all events
+#Loop through all events in the event history
 keep_looking=True
 old_event=None
+
 while keep_looking==True:
   history = dev.history(limit=100, kind='motion', older_than=old_event)
+
+  # If there are no more events, stop looking!
   if len(history) == 0:
     keep_looking=False;
+
+  # If there are events, loop through them and save each one.
   for recording in history:
+
     # pprint(recording)
+
+    # Keep track of the event id so we can look for older events in the next iteration
     old_event=recording['id']   
     timestr = recording['created_at'].strftime("%Y%m%d-%H%M%S")
     output_file = SCREENSHOT_DIRECTORY + '/motion-stickup1-' + timestr + '.mp4'
+
+    # Skip downloading if we have already downloaded them.
     if os.path.isfile(output_file):
       print output_file + ' exists, skipping...'
     else:
@@ -57,15 +72,4 @@ while keep_looking==True:
         filename= output_file,
         override=True)
 
-exit()
 
-
-
-history = dev.history(limit=10, kind='motion')
-for recording in history:
-    timestr = recording['created_at'].strftime("%Y%m%d-%H%M%S")
-
-    dev.recording_download(
-        recording['id'],
-        filename= SCREENSHOT_DIRECTORY + '/motion-stickup1-' + timestr + '.mp4',
-        override=True)
